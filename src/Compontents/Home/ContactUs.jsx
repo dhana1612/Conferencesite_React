b1c9a5b121d0e7d04968096605fe9c4649be86ce
Dhana <dhana121622@gmail.com>
2025-03-08 05:53:47 +0530

    commit

--- src/Compontents/Home/ContactUs.jsx
+++ src/Compontents/Home/ContactUs.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ContactUs() {
   return (
    <>
-      <div id="intro-example" className="img-fluid animated fadeIn" style={{ height: "30vh", marginTop: "80px", width: "100%", backgroundImage: "url('/public/Images/Photo22.jpg')", backgroundSize: "cover", backgroundPosition: "center",}}>
+      <div id="intro-example" className="img-fluid animated fadeIn" style={{ height: "30vh", marginTop: "80px", width: "100%", backgroundImage: "url('./Images/Photo22.jpg')", backgroundSize: "cover", backgroundPosition: "center",}}>
         <div className="mask w-100 h-100  bg-opacity-50 d-flex flex-column justify-content-center align-items-center">
           <div className="text-white text-start px-3 animated slideInDown">
             <h2 className="fw-bold display-5 text-uppercase text-light">Contact Us</h2>
